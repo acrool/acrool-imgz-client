9f5db9367838763f17eea6e351785e401e94b4c4
imagine10255 <imagine10255@gmail.com>
2024-10-03 21:27:19 +0800

    add test use docker

--- __test__/ImgzClient.spec.ts
+++ __test__/ImgzClient.spec.ts
@@ -3,55 +3,67 @@ import Docker from 'dockerode';
 import ImgzClient from '../src/ImgzNodeClient';
 import * as fs from 'fs';
 
-const docker = new Docker();
-const image = 'imagine10255/acrool-imgz-api:latest';
+describe('Test for mock server', () => {
+    let container: Docker.Container;
+    const timeout = 30 * 1000;
 
-// describe('Test for mock server', () => {
-//     let container: Docker.Container;
-//     const timeout = 30 * 1000;
-//
-//     beforeAll(async() => {
-//         // 在測試開始前啟動 Docker 容器
-//         container = await docker.createContainer({
-//             Image: image,
-//             Tty: true,
-//             ExposedPorts: {'3000/tcp': {}},
-//             HostConfig: {
-//                 PortBindings: {'3000/tcp': [{HostPort: '8082'}]},
-//             },
-//         });
-//
-//         await container.start();
-//
-//         await new Promise((resolve) => setTimeout(resolve, 10000));
-//         return container;
-//     }, timeout);
-//
-//     afterAll(async () => {
-//         // 測試結束後關閉 Docker 容器
-//         await container.stop();
-//         await container.remove();
-//     }, timeout);
-//
-//
-//     test('squash should compress an image and save it to the specified path', async () => {
-//         const imgzClient = new ImgzClient('http://localhost:8082');
-//
-//         const filePath = path.join(__dirname, '../../static/fixture.jpg');
-//         const uploadIOPath = path.join(__dirname, '../../static/losslessSquash.webp');
-//
-//         const options = {
-//             resize: {width: 250},
-//             quality: 90,
-//             timeout,
-//             uploadIOPath,
-//         };
-//         const result = await imgzClient
-//             .squashWebp(filePath, options);
-//
-//         expect(result).toEqual(uploadIOPath);
-//     }, timeout);
-// });
+    const docker = new Docker();
+    const image = 'imagine10255/acrool-imgz-api:latest';
+
+
+    beforeAll(async() => {
+        // 清理測試檔案
+        if (fs.existsSync(uploadIOPath)) {
+            fs.unlinkSync(uploadIOPath);
+        }
+        // 在測試開始前啟動 Docker 容器
+        container = await docker.createContainer({
+            Image: image,
+            Tty: true,
+            ExposedPorts: {'3000/tcp': {}},
+            HostConfig: {
+                PortBindings: {'3000/tcp': [{HostPort: '8082'}]},
+            },
+        });
+
+        await container.start();
+
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+        return container;
+    }, timeout);
+
+    afterAll(async () => {
+        // 測試結束後關閉 Docker 容器
+        await container.stop();
+        await container.remove();
+    }, timeout);
+
+
+    const filePath = path.join(__dirname, './assets/sample.jpg');
+    const uploadIOPath = path.join(__dirname, './out/sample.webp');
+    const uploadIOThumbPath = path.join(__dirname, './out/sample_thumb.webp');
+
+    test('squash should compress an image and save it to the specified path', async () => {
+        const imgzClient = new ImgzClient('http://localhost:8082');
+
+        const options = {
+            resize: {width: 500},
+            quality: 90,
+        };
+        const thumbOptions = {
+            resize: {width: 100},
+            quality: 80,
+        };
+        await imgzClient
+            .squashWebp(filePath)
+            .toSave(uploadIOThumbPath, thumbOptions)
+            .toSave(uploadIOPath, options)
+            .completeAll();
+
+        expect(fs.existsSync(uploadIOThumbPath)).toBe(true);
+        expect(fs.existsSync(uploadIOPath)).toBe(true);
+    }, timeout);
+});
 
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {default as ImgzClient} from './ImgzClient';
+export {default as ImgzNodeClient} from './ImgzNodeClient';
 export {default} from './ImgzClient';
 export * from './utils';
