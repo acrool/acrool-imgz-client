a01f023635ac4fa5648fff29cec70cd0495fe15f
imagine10255 <imagine10255@gmail.com>
2024-10-04 11:15:55 +0800

    add export types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {default as ImgzClient} from './ImgzClient';
 export {default as ImgzNodeClient} from './ImgzNodeClient';
 export {default} from './ImgzClient';
 export * from './utils';
+export * from './types';
