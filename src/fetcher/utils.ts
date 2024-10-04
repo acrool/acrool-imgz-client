e8e5497604e0e5bea814c8cf50fa913d5d5ae045
imagine10255 <imagine10255@gmail.com>
2024-10-04 14:22:26 +0800

    axios auto transfer FormData fail (boolean and nodejs file) #5

--- src/fetcher/utils.ts
+++ src/fetcher/utils.ts
@@ -2,6 +2,11 @@ import {AxiosResponse} from 'axios';
 
 import {SystemException} from '../exception';
 
+// 後端必須新增
+import FormData from 'form-data';
+import * as fs from 'node:fs';
+
+
 
 
 
@@ -10,7 +15,7 @@ import {SystemException} from '../exception';
  * @param response
  */
 export const getSystemError = (response: AxiosResponse) => {
-    const firstError = response.data;
+    const firstError = response?.data;
     return new SystemException({
         message: firstError?.message ?? response.statusText,
         code: firstError?.statusCode ?? `HTTP_CODE_${response.status}`,
@@ -18,3 +23,63 @@ export const getSystemError = (response: AxiosResponse) => {
     });
 };
 
+
+
+/**
+ * 將物件資料轉成 FormData
+ *
+ * 布林會自動轉成字串
+ * file 需要支援 File, Readable 型別
+ *
+ * ex: {
+ *     profile: {name: 'jack'}
+ * }
+ *
+ * @param data
+ */
+export function objToFormData(data: { [key: string]: any }): FormData {
+    const formData = new FormData();
+
+    const appendData = (whileData: any, parentKey = '') => {
+        
+        for (const [key, value] of Object.entries(whileData)) {
+            const formKey = parentKey ? `${parentKey}[${key}]` : key;
+            console.log('value', formKey, value);
+
+            if (Array.isArray(value)) {
+                value.forEach((item, index) => {
+                    if (typeof item === 'object' && item !== null) {
+                        appendData(item, `${formKey}[${index}]`);
+                    } else {
+                        formData.append(`${formKey}[${index}]`, formatValue(item));
+                    }
+                });
+            } else if (typeof value === 'object' && value !== null) {
+                // 檢查是否為 File 或 Readable
+                if(typeof File !== 'undefined' && value instanceof File){
+                    formData.append(formKey, new Blob(value as any));
+
+                } else if (typeof fs.ReadStream !== 'undefined' && value instanceof fs.ReadStream) {
+                    formData.append(formKey, value);
+
+                } else {
+                    appendData(value, formKey);
+                }
+            } else if (value !== undefined && value !== null) {
+                formData.append(formKey, formatValue(value));
+            }
+        }
+    };
+
+    // 處理布林值和其他基本類型
+    const formatValue = (value: any) => {
+        if (typeof value === 'boolean') {
+            return value.toString();  // 布林值轉成字串
+        }
+        return value;
+    };
+
+    appendData(data);
+
+    return formData;
+}
