c4bca10fd92d9c42ced82bd4340ada302a38c89e
imagine10255 <imagine10255@gmail.com>
2024-12-12 20:55:31 +0800

    fix statusText undefined

--- src/fetcher/utils.ts
+++ src/fetcher/utils.ts
@@ -18,7 +18,7 @@ export const getSystemError = (response: AxiosResponse) => {
     const firstError = response?.data;
     return new SystemException({
         message: firstError?.message ?? response?.statusText ?? JSON.stringify(response),
-        code: firstError?.statusCode ?? `HTTP_CODE_${response.status}`,
+        code: firstError?.statusCode ?? response?.status ? `HTTP_CODE_${response.status}`: 'NO_CODE',
         // path: firstError?.path
     });
 };
